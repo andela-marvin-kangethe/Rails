4f83af3649a23dc688163e726b59a0470802fa27
izlyr <fuzzkillofficial@gmail.com>
2017-02-25 16:59:48 +0300

    feat(image)Implement nav bar and image card

--- PicHippie/app/helpers/homepage_helper.rb
+++ PicHippie/app/helpers/homepage_helper.rb
@@ -1,2 +1,9 @@
 module HomepageHelper
+	def get_image(url)
+		id = ''
+		for values in url.scan(/\d/).map(&:to_i)
+			id+=values.to_s
+		end
+		image_url = "https://images.pexels.com/photos/#{id}/pexels-photo-#{id}.jpeg"
+	end
 end
